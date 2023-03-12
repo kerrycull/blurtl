a90c4a5a6b95153ae406782691bbffce966ff640
Kerry Cull <kerry@Kerrys-MacBook-Pro.local>
2023-03-12 21:29:19 +0000

    fixed updoots garmmoonnnn

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import "../Article.css";
 import { doc, updateDoc, getDoc } from "firebase/firestore";
 import { db } from "../firebase";
